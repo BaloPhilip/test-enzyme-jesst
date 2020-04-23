72697fb7e999f7ee220dab0100e0d133845158b1
Philip Balo <f.g.balo@gmail.com>
2020-04-23 20:17:57 +0300

    Return to the function component

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -7,24 +7,21 @@ const notificationType = {
   error: "alert alert-danger"
 };
 
-export default class Notification extends React.Component {
-  render () {
-    const { message, type, children } = this.props;
-    
-    if (!message) {
-      return null;
-    }
-  
-    return (
-      <div className={notificationType[type || "message"]}>
-        <p>{message}</p>
-        {
-          children
-          ? children
-          : null
-        }
-      </div>
-    );
+const Notification = ({ message, type, children }) => {
+  if (!message) {
+    return null;
   }
-  
+
+  return (
+    <div className={notificationType[type || "message"]}>
+      <p>{message}</p>
+      {
+        children
+        ? children
+        : null
+      }
+    </div>
+  );
 };
+
+export default Notification;
